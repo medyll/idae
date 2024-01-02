715c23bbb9ac388ab26a44bf420e2373407eb794
medyll <43032494+medyll@users.noreply.github.com>
2024-01-02 19:19:38 +0100

    dev: Add @playwright/test and playwright as devDependencies

---
+++ src/lib/MutationObserverInterface.ts
@@ -0,0 +1,202 @@
+type HtmluDomElement = {};
+
+type MutationRecordDetails = {
+	spawn: Element[];
+	trashed: Element[];
+	reallocated: Element[];
+	attributeChanged: Record<string, Element[]>;
+	elementChanged: Record<string, Element[]>;
+	oldAttribute: any; // à définir en fonction de vos besoins
+	lastParentNode: any; // à définir en fonction de vos besoins
+};
+
+interface Selector {
+	element: string | Node | NodeList;
+	mutations: {
+		attributes?: string;
+	};
+}
+
+type MutationHandlerCallback = (
+	mutations: MutationRecord[],
+	observer: MutationObserver
+) => Record<
+	Partial<MutationRecordType>,
+	(mutation: MutationRecord, observer: MutationObserver) => any
+>;
+
+type AttachedElement = {
+	selectors: Selector | Selector[];
+	mutationCb: MutationHandlerCallback;
+	mutationObserverParams: MutationObserverInit;
+};
+
+type AttachOptionsType = AttachedElement | AttachedElement[];
+
+class HtmluDomUtils {
+	static forceArrayType<T>(value: T | T[]): T[] {
+		return Array.isArray(value) ? value : [value];
+	}
+}
+
+class MutationsHandler {
+	observedNode: ObservedElement;
+
+	constructor(observedNode: ObservedElement) {
+		this.observedNode = observedNode;
+	}
+
+	handleMutation(mutations: MutationRecord[], observer: MutationObserver) {
+		for (const mutation of mutations) {
+			if (
+				mutation.type &&
+				this.observedNode.mutationConfig[mutation.type] &&
+				typeof this.observedNode?.mutationCallback == 'function'
+			) {
+				console.log(this.observedNode.mutationCallback(mutations, observer)[mutation.type]);
+			}
+		}
+	}
+}
+
+const defaultMutationConfig: MutationObserverInit = {
+	attributeFilter: undefined,
+	childList: false,
+	subtree: false
+};
+
+/**
+ * Represents an observed element for the MutationObserverInterface.
+ */
+class ObservedElement {
+	public targetNode!: Node;
+	public mutationConfig!: MutationObserverInit;
+	public mutationCallback: MutationHandlerCallback;
+
+	constructor(
+		element: Node,
+		mutationConfig: MutationObserverInit,
+		actions: MutationHandlerCallback
+	) {
+		this.targetNode = element;
+		this.mutationConfig = this.updateDefaultConfig(mutationConfig);
+		this.mutationCallback = actions;
+
+		return this;
+	}
+
+	updateDefaultConfig(params: MutationObserverInit) {
+		return { ...defaultMutationConfig, ...params };
+	}
+}
+
+class HtmluEvent {
+	private observedElement!: ObservedElement;
+	private observer!: MutationObserver;
+
+	constructor(observedNode: ObservedElement) {
+		// should i stack the observed elements in a specific object /class
+		// yes
+		this.observedElement = observedNode;
+	}
+
+	observe() {
+		try {
+			/** create callback */
+			const callBackHandler = new MutationsHandler(this.observedElement);
+			/** create observer */
+			this.observer = new MutationObserver(callBackHandler.handleMutation.bind(callBackHandler));
+			/** observe */
+			this.observer.observe(this.observedElement.targetNode, this.observedElement.mutationConfig);
+		} catch (error) {
+			console.error('Error in observe method', error);
+		}
+	}
+
+	disconnect() {
+		this.observer.disconnect();
+	}
+
+	addEvent(eventType: string, callback: EventListenerOrEventListenerObject) {
+		this.observedElement.targetNode.addEventListener(eventType, callback);
+	}
+
+	removeEvent(eventType: string, callback: EventListenerOrEventListenerObject) {
+		this.observedElement.targetNode.removeEventListener(eventType, callback);
+	}
+}
+
+/**
+ * Represents a loader for the HtmluDom library.
+ */
+export class HtmluDomLib {
+	private static instance: HtmluDomLib;
+
+	private constructor() {}
+
+	/**
+	 * Returns the singleton instance of the HtmluDomLoader.
+	 * @returns The singleton instance of the HtmluDomLoader.
+	 */
+	public static getInstance(): HtmluDomLib {
+		if (!HtmluDomLib.instance) {
+			HtmluDomLib.instance = new HtmluDomLib();
+		}
+		return HtmluDomLib.instance;
+	}
+
+	/**
+	 * Attaches the HtmluDom library to the specified elements.
+	 * @param opts - The options for attaching the library.
+	 */
+	public attach(opts: AttachOptionsType | AttachOptionsType[]) {
+		const optionsArray = HtmluDomUtils.forceArrayType<AttachOptionsType>(opts);
+		if (typeof window == 'undefined' || !optionsArray) return;
+
+		for (const option of optionsArray as AttachedElement[]) {
+			const selectorsArray = HtmluDomUtils.forceArrayType<Selector>(option.selectors);
+
+			for (const elementSelector of selectorsArray) {
+				const elements = this.getSelectorElements(elementSelector); // document.querySelectorAll(elementSelector.element);
+
+				for (const element of elements) {
+					const targetNode = new ObservedElement(
+						element,
+						option.mutationObserverParams,
+						option.mutationCb
+					);
+
+					const core = new HtmluEvent(targetNode);
+					core.observe();
+
+					this.storeObservable(core);
+				}
+			}
+		}
+	}
+
+	private getSelectorElements({ element }: Selector): Node[] {
+		let elementList: Node[] = [];
+		if (element instanceof Node) {
+			elementList = [element];
+		} else if (element instanceof NodeList) {
+			elementList = Array.from(element);
+		} else if (typeof element == 'string') {
+			elementList = Array.from(document.querySelectorAll(element));
+		}
+		return elementList;
+	}
+	/**
+	 * Detaches the HtmluDom library from the specified elements.
+	 * @param args - The options for detaching the library.
+	 */
+	static detach(args: any /* to define */) {}
+
+	/**
+	 * Stores the observable elements.
+	 * @param element - The observable element.
+	 */
+	private storeObservable(element: HtmluEvent) {}
+}
+
+export const HtmluDom = HtmluDomLib.getInstance();
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,46 @@
-<h1>Welcome to your library project</h1>
-<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import { HtmluDom } from '$lib/MutationObserverInterface.js';
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		let timer: NodeJS.Timer;
+
+		HtmluDom.attach({
+			selectors: [{ element: '[data-widget]', mutations: { attributes: '[lang]' } }],
+			mutationCb: (mutations, observer) => {
+				return {
+					attributes: (mutation: MutationRecord, observer: MutationObserver) => {
+						console.log(mutation);
+					},
+					childList: (mutation: MutationRecord, observer: MutationObserver) => {
+						console.log(mutation);
+					},
+					characterData: (mutation: MutationRecord, observer: MutationObserver) => {
+						console.log(mutation);
+					}
+				};
+			},
+			mutationObserverParams: {
+				attributes: true
+				// attributeFilter: ['lang']
+			}
+		});
+
+		document.querySelector('[data-widget]').setAttribute('lang', Math.random(0, 200));
+
+		timer = setInterval(() => {
+			// @ts-ignore
+			document.querySelector('[data-widget]').setAttribute('lang', Math.random(0, 200));
+		}, 10000);
+
+		return () => {
+			clearInterval(timer);
+			console.log('unmount');
+		};
+	});
+</script>
+
+<div id="widget" lang="fr" data-widget>
+	<h1>Page</h1>
+	<p>Page content</p>
+</div>
