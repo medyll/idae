566325dbece3f439ef12f279d02b0438f0cb6b1e
medyll <43032494+medyll@users.noreply.github.com>
2024-03-31 21:07:13 +0200

    feat: taskbar migration

--- src/lib/ui/taskbar/Taskbar.svelte
+++ src/lib/ui/taskbar/Taskbar.svelte
@@ -1,14 +1,30 @@
 <script lang="ts">
-	let className = '';
-	export { className as class };
+	import type { CommonProps } from '$lib/types/index.js';
+	import Slotted from '$lib/utils/slot/Slotted.svelte';
+	import type { Snippet } from 'svelte';
+
+	type TaskbarProps = CommonProps & {
+		/** className off the root component */
+		class?: string;
+
+		/** element root HTMLDivElement props */
+		element?: HTMLDivElement | null;
+
+		/** slots for the taskbar */
+		slots?: {
+			taskBarLeft?: Snippet;
+			taskBarRight?: Snippet;
+		};
+	};
+	let { class: className, children, slots = {}, ...restProps }: TaskbarProps = $props();
 </script>
 
-<div class="taskbar flex-h flex-align-middle {className}" {...$$restProps}>
-	<slot name="taskBarLeft" />
+<div class="taskbar flex-h flex-align-middle {className}" {...restProps}>
+	<Slotted slotted={slots.taskBarLeft} />
 	<div class="flex-main">
-		<slot />
+		<Slotted slotted={children} />
 	</div>
-	<slot name="taskBarRight" />
+	<Slotted slotted={slots.taskBarRight} />
 </div>
 
 <style lang="scss">
