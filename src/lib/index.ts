594333fa96c176b9594bab7a257f8d7282f5c421
medyll <43032494+medyll@users.noreply.github.com>
2024-07-06 15:04:44 +0200

    chore: added indexes

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -6,5 +6,6 @@ export * from '$lib/scripts/operators/operators.js';
 export * from '$lib/scripts/path/pathResolver.js';
 export * from '$lib/scripts/query/query.js';
 export * from '$lib/scripts/resultSet/Resultset.js';
+export * from '$lib/scripts/state/idbqlEvent.svelte.js';
 export * from '$lib/scripts/state/idbstate.svelte.js';
 export * from '$lib/scripts/types.js';
