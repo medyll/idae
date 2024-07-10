895d2cd6c7ecf3aa23fa445444544fe4bcd4f4ac
medyll <43032494+medyll@users.noreply.github.com>
2024-07-10 20:24:04 +0200

    Squashed 'packages/htmludom/' content from commit 45fa0d2
    git-subtree-dir: packages/htmludom
    git-subtree-split: 45fa0d2177ec42db0aefcaf0c4dee0b127433193

---
+++ src/lib/HtmluDom.test.ts
@@ -0,0 +1,66 @@
+import puppeteer from 'puppeteer';
+import { afterAll, beforeAll, describe, expect, it } from 'vitest';
+import { Htmlu } from './HtmluDom.js';
+
+describe('HtmluDomLib', () => {
+	let browser: puppeteer.Browser;
+	let page: puppeteer.Page;
+
+	beforeAll(async () => {
+		browser = await puppeteer.launch();
+		page = await browser.newPage();
+		await page.goto('http://localhost:3000'); // Replace with the URL of your application
+	});
+
+	afterAll(async () => {
+		await browser.close();
+	});
+
+	it('should attach the library to the specified elements', async () => {
+		// Write your test logic here
+		// Example:
+		await page.evaluate(() => {
+			const options = {
+				defaultAttributeFilter: ['data-testid'],
+				useSubtree: true,
+				debounceTime: 100,
+				errorHandler: (error) => {
+					throw new Error(`HtmluDomLib error: ${error}`);
+				}
+			};
+
+			Htmlu.updateOptions(options);
+
+			Htmlu.attach({
+				selectors: [{ element: '#widget', mutations: { attributes: ['lang'] } }],
+				selectorCallback: (element, mutations, observer) => {
+					// Your callback logic here
+				},
+				observerParameters: { attributeFilter: ['lang'], subtree: true }
+			});
+		});
+
+		// Assert the expected behavior
+		const activeObserversCount = await page.evaluate(() => {
+			return Htmlu.getActiveObserversCount();
+		});
+
+		expect(activeObserversCount).toBe(1);
+	});
+
+	it('should detach the library from the specified elements', async () => {
+		// Write your test logic here
+		// Example:
+		await page.evaluate(() => {
+			Htmlu.detach('#widget');
+		});
+
+		// Assert the expected behavior
+		// Example:
+		const activeObserversCount = await page.evaluate(() => {
+			return Htmlu.getActiveObserversCount();
+		});
+
+		expect(activeObserversCount).toBe(0);
+	});
+});
