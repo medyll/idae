00e7635ea06b695eff10b822a27090aeff7dafcb
medyll <43032494+medyll@users.noreply.github.com>
2024-07-11 15:24:21 +0200

    chore(data-query): added operators

---
+++ packages/data-query/src/lib/query/query.ts
@@ -0,0 +1,42 @@
+/* src\lib\scripts\query\query.ts */
+import { Operators } from '$lib/operators/operators.js';
+
+import { getResultset } from '$lib/resultSet/Resultset.js';
+
+import type { Operator, Where } from '$lib/types.js';
+
+export class Query<T extends object> {
+	data: T[];
+	constructor(data: T[]) {
+		this.data = data;
+		getResultset(this.data ?? []);
+	}
+
+	where(qy: Where<T>) {
+		this.data = this.data.filter((item) => this.matchesQuery(item, qy));
+		return getResultset(this.data);
+	}
+
+	private matchesQuery(item: T, query: Where<T>): boolean {
+		if (typeof query !== 'object' || query === null) {
+			return false;
+		}
+
+		return Object.entries(query).every(([key, value]) => {
+			if (Operators.operators.includes(key as Operator)) {
+				return Operators.filters(key as Operator, 'eq', value, [item]).length > 0;
+			} else {
+				return this.matchesField(item, key as keyof T, value);
+			}
+		});
+	}
+
+	private matchesField(item: T, field: keyof T, condition: any): boolean {
+		if (typeof condition === 'object' && condition !== null && !Array.isArray(condition)) {
+			return Object.entries(condition).every(
+				([op, val]) => Operators.filters(field, op as Operator, val, [item]).length > 0
+			);
+		}
+		return Operators.filters(field, 'eq', condition, [item]).length > 0;
+	}
+}
---
+++ packages/data-query/src/lib/resultSet/resultset.test.ts
@@ -0,0 +1,87 @@
+import { describe, beforeEach, it, expect } from "vitest";
+import { getResultset } from "./Resultset.js";
+
+describe("getResultset", () => {
+  let data: any[];
+
+  beforeEach(() => {
+    data = [
+      { id: 1, name: "John", age: 25, metadata: { order: 1 } },
+      { id: 2, name: "Jane", age: 30, metadata: { order: 2 } },
+      { id: 3, name: "Bob", age: 35, metadata: { order: 3 } },
+      { id: 4, name: "Alice", age: 40, metadata: { order: 4 } },
+    ];
+  });
+
+  it("should set options and apply sorting", () => {
+    const resultSet = getResultset(data);
+    const options = { sort: { age: "asc" } };
+    //@ts-ignore
+    const result = resultSet.setOptions(options);
+    expect(result).toBe(resultSet);
+    expect(resultSet).toEqual([
+      { id: 1, name: "John", age: 25, metadata: { order: 1 } },
+      { id: 2, name: "Jane", age: 30, metadata: { order: 2 } },
+      { id: 3, name: "Bob", age: 35, metadata: { order: 3 } },
+      { id: 4, name: "Alice", age: 40, metadata: { order: 4 } },
+    ]);
+  });
+
+  it("should set options and apply sorting", () => {
+    const resultSet = getResultset(data);
+    const options = { sort: { age: "desc" } };
+    //@ts-ignore
+    const result = resultSet.setOptions(options);
+    expect(result).toBe(resultSet);
+    expect(resultSet).toEqual([
+      { id: 4, name: "Alice", age: 40, metadata: { order: 4 } },
+      { id: 3, name: "Bob", age: 35, metadata: { order: 3 } },
+      { id: 2, name: "Jane", age: 30, metadata: { order: 2 } },
+      { id: 1, name: "John", age: 25, metadata: { order: 1 } },
+    ]);
+  });
+
+  /*   it("should set options and apply grouping", () => {
+    const resultSet = getResultset(data);
+    const options = { groupBy: "age" };
+    const result = resultSet.setOptions(options);
+    expect(result).toBe(resultSet);
+    expect(resultSet).toEqual({
+      25: [{ id: 1, name: "John", age: 25, metadata: { order: 1 } }],
+      30: [{ id: 2, name: "Jane", age: 30, metadata: { order: 2 } }],
+      35: [{ id: 3, name: "Bob", age: 35, metadata: { order: 3 } }],
+      40: [{ id: 4, name: "Alice", age: 40, metadata: { order: 4 } }],
+    });
+  }); */
+
+  it("should sort the data in ascending order", () => {
+    const resultSet = getResultset(data);
+    const sortedData = resultSet.sortBy({ age: "desc" });
+    expect(sortedData).toEqual([
+      { id: 4, name: "Alice", age: 40, metadata: { order: 4 } },
+      { id: 3, name: "Bob", age: 35, metadata: { order: 3 } },
+      { id: 2, name: "Jane", age: 30, metadata: { order: 2 } },
+      { id: 1, name: "John", age: 25, metadata: { order: 1 } },
+    ]);
+  });
+
+  it("should group the data by a single field", () => {
+    const resultSet = getResultset(data);
+    const groupedData = resultSet.groupBy("age");
+    expect(groupedData).toEqual({
+      25: [{ id: 1, name: "John", age: 25, metadata: { order: 1 } }],
+      30: [{ id: 2, name: "Jane", age: 30, metadata: { order: 2 } }],
+      35: [{ id: 3, name: "Bob", age: 35, metadata: { order: 3 } }],
+      40: [{ id: 4, name: "Alice", age: 40, metadata: { order: 4 } }],
+    });
+  });
+
+  it("should get the specified page of data", () => {
+    const resultSet = getResultset(data);
+    const pageData = resultSet.getPage(2, 1);
+    expect(pageData).toEqual([
+      { id: 1, name: "John", age: 25, metadata: { order: 1 } },
+      { id: 2, name: "Jane", age: 30, metadata: { order: 2 } },
+    ]);
+  });
+});
