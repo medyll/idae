570407eb2e99393a110d2c7d1a4f242b22d9e9bf
medyll <43032494+medyll@users.noreply.github.com>
2024-07-26 22:37:44 +0200

    chore(idae-api): Add database middleware for connecting to the database

---
+++ packages/idae-api/src/lib/middleware/databaseMiddleware.ts
@@ -0,0 +1,12 @@
+// packages\idae-api\src\lib\middleware\databaseMiddleware.ts
+
+import type { Request, Response, NextFunction } from 'express';
+import databaseManager from '../engine/DatabaseManager';
+
+export const connectToDatabase = async (req: Request, res: Response, next: NextFunction) => {
+	try {
+		await databaseManager.connectToDatabase(req, res, next);
+	} catch (error) {
+		next(error);
+	}
+};
