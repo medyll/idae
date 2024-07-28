a12251414647ff44751c6782b7821f87e7fad687
medyll <43032494+medyll@users.noreply.github.com>
2024-07-28 07:43:47 +0200

    chore(idae-api): Refactor databaseMiddleware to store database connection details in request object

--- packages/idae-api/src/lib/middleware/databaseMiddleware.ts
+++ packages/idae-api/src/lib/middleware/databaseMiddleware.ts
@@ -4,12 +4,13 @@ import type { Request, Response, NextFunction } from 'express';
 import databaseManager from '$lib/engine/DatabaseManager';
 
 export const databaseMiddleware = async (req: Request, res: Response, next: NextFunction) => {
-	console.log('Middleware: Connect to database');
-	console.log('Middleware: req.params', req.params);
 	try {
-		// Vérifiez si req.params.collectionName existe, sinon utilisez une valeur par défaut
-		// const collectionName = req.params.collectionName || 'default';
-		await databaseManager.connectToDatabase(req, res, next);
+		const { collectionName, dbName, connection } = await databaseManager.connectToDatabase(req);
+
+		req.collectionName = collectionName;
+		req.dbConnection = connection;
+		req.dbName = dbName;
+		next();
 	} catch (error) {
 		console.error('Error in database connection middleware:', error);
 		next(error);
