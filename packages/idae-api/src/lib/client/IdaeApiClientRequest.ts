5dd24bfdea1b1cf15fb1e5262022c50eaead073c
medyll <43032494+medyll@users.noreply.github.com>
2024-07-29 03:58:52 +0200

    chore(idae-api): Add IdaeApiClientRequest class for making API requests

---
+++ packages/idae-api/src/lib/client/IdaeApiClientRequest.ts
@@ -0,0 +1,87 @@
+// packages\idae-api\src\lib\client\IdaeApiClientRequest.ts
+import { IdaeApiClientConfigCore } from './IdaeApiClientConfig';
+
+type RequestParams = Record<string, unknown>;
+type HttpMethod = 'GET' | 'POST' | 'PUT' | 'DELETE' | 'PATCH';
+type RouteNamespace = `methods/${'dbs' | 'collections'}` | undefined;
+
+interface UrlParams {
+	dbName?: string;
+	collectionName?: string;
+	slug?: string;
+	params?: Record<string, string>;
+	routeNamespace?: RouteNamespace;
+}
+
+interface RequestOptions<T> extends UrlParams {
+	baseUrl?: string;
+	method?: HttpMethod;
+	body?: T;
+	headers?: RequestInit['headers'];
+}
+
+class IdaeApiClientRequest {
+	private baseUrl: string;
+	private clientConfig: IdaeApiClientConfigCore;
+
+	constructor(clientConfig: IdaeApiClientConfigCore) {
+		this.clientConfig = clientConfig;
+		this.baseUrl = `${this.clientConfig.method}:${this.clientConfig.separator}${this.clientConfig.host}:${this.clientConfig.port}`;
+	}
+
+	async doRequest<T, R = any>({
+		baseUrl = this.baseUrl,
+		method = 'GET',
+		body,
+		headers = {
+			'Content-Type': 'application/json'
+		},
+		dbName = this.clientConfig.defaultDb,
+		collectionName,
+		slug,
+		params,
+		routeNamespace
+	}: RequestOptions<T>): Promise<R> {
+		const url = this.buildUrl({
+			routeNamespace,
+			dbName,
+			collectionName,
+			slug,
+			params
+		}).replace('//', '/');
+
+		const response = await fetch(`${baseUrl}${url}`, {
+			method,
+			headers,
+			body: body ? JSON.stringify(body) : undefined
+		});
+
+		if (!response.ok) {
+			throw new Error(`HTTP error! status: ${response.status}`);
+		}
+		try {
+			return response.json();
+		} catch (e) {
+			throw new Error(`Invalid returned type`);
+		}
+	}
+
+	private buildUrl({ dbName, collectionName, slug, params, routeNamespace }: UrlParams): string {
+		const urls: string[] = [`/`];
+		if (routeNamespace) urls.push(routeNamespace);
+		if (dbName ?? collectionName) urls.push([dbName, collectionName].filter((x) => x).join('.'));
+		if (slug) urls.push(slug);
+		if (params) urls.push(`?${new URLSearchParams(params).toString()}`);
+
+		return urls.join('/').replace('//', '/');
+	}
+}
+
+export {
+	IdaeApiClientRequest,
+	type RequestParams,
+	type HttpMethod,
+	type RouteNamespace,
+	type UrlParams,
+	type RequestOptions
+};
