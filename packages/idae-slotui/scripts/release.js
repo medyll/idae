d59e81bcf57289570110e83934f67582d74665dc
medyll <43032494+medyll@users.noreply.github.com>
2024-08-10 23:49:47 +0200

    fix(idae-slotui): incorrect load of css files

--- packages/idae-slotui/scripts/release.js
+++ packages/idae-slotui/scripts/release.js
@@ -1,7 +1,7 @@
 import {SCSSConverter} from './release-css.js';
 import { MakeLibIndex } from '../../shared/makeLibIndexRoot.js';
 
-const converter = new SCSSConverter('./src/lib', './src/lib/_css');
+const converter = new SCSSConverter('./src/lib', './src/lib/slotui-css');
 
 
 converter.processAllFiles();
