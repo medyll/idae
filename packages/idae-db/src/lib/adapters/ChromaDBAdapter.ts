2591029c364f074330cc509a066b76cab8db92b9
medyll <43032494+medyll@users.noreply.github.com>
2024-08-20 04:17:48 +0200

    chore(idae-db): Add ChromaDBAdapter implementation

---
+++ packages/idae-db/src/lib/adapters/ChromaDBAdapter.ts
@@ -0,0 +1,70 @@
+// packages/idae-db/lib/adapters/ChromaDBAdapter.ts
+
+import { Collection, type Document, type ChromaClient } from 'chromadb';
+import type { IdaeDbParams, IdaeDbAdapter } from '../types.js';
+import { IdaeDbConnection } from '../IdaeDbConnection.js';
+
+export class ChromaDBAdapter<T extends Document = Document> implements IdaeDbAdapter<T> {
+	private collection!: Collection;
+	private client: ChromaClient;
+
+	constructor(
+		private collectionName: string,
+		private connection: IdaeDbConnection
+	) {
+		this.client = this.connection.getDb() as ChromaClient; // Assuming getDb() returns ChromaClient
+		return this._init();
+	}
+
+	private _init() {
+		this.collection = this.client
+			.getOrCreateCollection({ name: this.collectionName })
+			.then((collection) => {
+				return collection;
+			});
+		return this;
+	}
+
+	async findById(id: string) {
+		return this.collection.get([id]);
+	}
+
+	async find(params: IdaeDbParams<T>) {
+		const { query = {}, limit } = params;
+		// ChromaDB uses vector similarity search, so we need to adapt the query
+		// This is a simplified example and may need to be adjusted based on your specific use case
+		return this.collection.query(query.vector || [], limit);
+	}
+
+	async findOne(params: IdaeDbParams<T>) {
+		const results = await this.find({ ...params, limit: 1 });
+		return results.length > 0 ? results[0] : null;
+	}
+
+	async update(id: string, updateData: Partial<T>) {
+		// ChromaDB doesn't have a direct update method, so we need to delete and re-add
+		await this.deleteById(id);
+		return this.collection.add(id, updateData.vector, updateData.metadata);
+	}
+
+	async updateWhere(params: IdaeDbParams<T>, updateData: Partial<T>) {
+		throw new Error('updateWhere not supported in ChromaDB');
+	}
+
+	async deleteById(id: string) {
+		return this.collection.delete([id]);
+	}
+
+	async deleteWhere(params: IdaeDbParams<T>): Promise<{ deletedCount?: number }> {
+		throw new Error('deleteWhere not supported in ChromaDB');
+	}
+
+	// Additional ChromaDB specific methods
+	async addItem(id: string, vector: number[], metadata?: object) {
+		return this.collection.add(id, vector, metadata);
+	}
+
+	async similaritySearch(vector: number[], k: number = 10) {
+		return this.collection.query(vector, k);
+	}
+}
--- packages/idae-db/src/lib/adapters/MongoDBAdapter.ts
+++ packages/idae-db/src/lib/adapters/MongoDBAdapter.ts
@@ -1,106 +1,105 @@
 // packages\idae-db\src\lib\adapters\MongoDBAdapter.ts
 
-import dotenv from "dotenv";
-import type {
-  IdaeDbParams,
-  IdaeDbAdapter,
-  IdaeDbParamsSortOptions,
-} from "../types.js";
+import dotenv from 'dotenv';
+import type { IdaeDbParams, IdaeDbAdapter, IdaeDbParamsSortOptions } from '../types.js';
 
 import {
-  Document,
-  type Filter,
-  type ClientSession,
-  type IndexSpecification,
-  type CreateIndexesOptions,
-} from "mongodb";
-import { IdaeDbConnection } from "../IdaeDbConnection.js";
-import { IdaeDBModel } from "../IdaeDBModel.js";
+	type Document,
+	type Filter,
+	type UpdateOptions,
+	type IndexSpecification,
+	type CreateIndexesOptions
+} from 'mongodb';
+import { IdaeDbConnection } from '../IdaeDbConnection.js';
+import { IdaeDBModel } from '../IdaeDBModel.js';
 
 // Load environment variables
 dotenv.config();
 
 // MongoDB Adapter
-export class MongoDBAdapter<T extends Document> implements IdaeDbAdapter<T> {
-  private model: IdaeDBModel<T>;
-  private connection: IdaeDbConnection;
-  private fieldId;
+export class MongoDBAdapter<T extends Document = Document> implements IdaeDbAdapter<T> {
+	private model: IdaeDBModel<T>;
+	private connection: IdaeDbConnection;
+	private fieldId: string;
 
-  constructor(collection: string, connection: IdaeDbConnection) {
-    // just in case
-    collection = collection.split(".")[1] ?? collection.split(".")[0];
+	constructor(collection: string, connection: IdaeDbConnection) {
+		// just in case
+		collection = collection.split('.')[1] ?? collection.split('.')[0];
 
-    this.connection = connection;
-    this.model = this.connection.model<T>(collection);
-    this.fieldId = this.model.fieldId;
-  }
+		this.connection = connection;
+		this.model = this.connection.getModel<T>(collection);
+		this.fieldId = this.model.fieldId;
+	}
 
-  async createIndex(
-    fieldOrSpec: IndexSpecification,
-    options?: CreateIndexesOptions
-  ): Promise<string> {
-    return this.model.collection.createIndex(fieldOrSpec, options);
-  }
-  async findById(id: string) {
-    return this.model.collection.find(
-      { [this.fieldId]: id },
-      { hint: this.fieldId }
-    );
-  }
+	async createIndex(
+		fieldOrSpec: IndexSpecification,
+		options?: CreateIndexesOptions
+	): Promise<string> {
+		return this.model.collection.createIndex(fieldOrSpec, options);
+	}
+	async findById(id: string) {
+		return this.model.collection.find({ [this.fieldId]: id }, { hint: this.fieldId });
+	}
 
-  async find(params: IdaeDbParams<T>) {
-    const { query = {}, sortBy, limit, skip } = params;
-    const sortOptions: IdaeDbParamsSortOptions = this.parseSortOptions(sortBy);
+	async find(params: IdaeDbParams<T>) {
+		const { query = {}, sortBy, limit, skip } = params;
+		const sortOptions: IdaeDbParamsSortOptions = this.parseSortOptions(sortBy);
 
-    return await this.model.collection
-      .find(query as Filter<T>)
-      .sort(sortOptions)
-      .limit(Number(limit) || 0)
-      .skip(Number(skip) || 0);
-  }
+		return await this.model.collection
+			.find(query as Filter<T>)
+			.sort(sortOptions)
+			.limit(Number(limit) || 0)
+			.skip(Number(skip) || 0);
+	}
 
-  findOne(params: IdaeDbParams<T>) {
-    return this.model.collection.findOne(params.query);
-  }
+	findOne(params: IdaeDbParams<T>) {
+		return this.model.collection.findOne(params.query);
+	}
 
-  async update(id: string, updateData: Partial<T>) {
-    return this.model.collection.updateMany(
-      { [this.fieldId]: id },
-      updateData,
-      {
-        upsert: true,
-      }
-    );
-  }
+	async create(data: Partial<T>) {
+		console.log('fieldId', this.fieldId);
+		// create auto_increment
+		const id = await this.model.getNextIncrement();
+		// ensure Index i set
+		return this.model.collection.updateOne(
+			{ [this.fieldId]: id },
+			{ $set: { ...data } },
+			{
+				upsert: true
+			}
+		);
+	}
+	async update(id: string, updateData: Partial<T>, options?: UpdateOptions) {
+		console.log('fieldId', this.fieldId);
+		return this.model.collection.updateMany(
+			{ [this.fieldId]: id },
+			{ $set: { ...updateData } },
+			options
+		);
+	}
 
-  async updateWhere(params: IdaeDbParams<T>, updateData: Partial<T>) {
-    return this.model.collection.updateMany(params.query, updateData, {
-      upsert: true,
-    });
-  }
+	async updateWhere(params: IdaeDbParams<T>, updateData: Partial<T>, options?: UpdateOptions = {}) {
+		return this.model.collection.updateMany(params.query, updateData, options);
+	}
 
-  async deleteById(id: string) {
-    return this.model.collection.deleteMany({ _id: { $eq: id } });
-  }
+	async deleteById(id: string | number) {
+		return this.model.collection.deleteMany({ [this.fieldId]: id });
+	}
 
-  async deleteWhere(
-    params: IdaeDbParams<T>
-  ): Promise<{ deletedCount?: number }> {
-    const result = await this.model.collection.deleteMany(
-      params.query as Filter<T>
-    );
-    return { deletedCount: result.deletedCount };
-  }
+	async deleteWhere(params: IdaeDbParams<T>): Promise<{ deletedCount?: number }> {
+		const result = await this.model.collection.deleteMany(params.query as Filter<T>);
+		return { deletedCount: result.deletedCount };
+	}
 
-  private parseSortOptions(sortBy?: string): IdaeDbParamsSortOptions {
-    const sortOptions: IdaeDbParamsSortOptions = {};
-    if (sortBy) {
-      const sortFields = sortBy.split(",");
-      sortFields.forEach((field) => {
-        const [key, order] = field.split(":");
-        sortOptions[key] = order === "desc" ? -1 : 1;
-      });
-    }
-    return sortOptions;
-  }
+	private parseSortOptions(sortBy?: string): IdaeDbParamsSortOptions {
+		const sortOptions: IdaeDbParamsSortOptions = {};
+		if (sortBy) {
+			const sortFields = sortBy.split(',');
+			sortFields.forEach((field) => {
+				const [key, order] = field.split(':');
+				sortOptions[key] = order === 'desc' ? -1 : 1;
+			});
+		}
+		return sortOptions;
+	}
 }
