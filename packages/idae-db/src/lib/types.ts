0b255af9fc3e1fc8738678622cca7551a89ad4de
medyll <43032494+medyll@users.noreply.github.com>
2024-08-22 02:16:55 +0200

    chore(idae-db): Add transaction support and database connection management methods

--- packages/idae-db/src/lib/types.ts
+++ packages/idae-db/src/lib/types.ts
@@ -10,13 +10,18 @@ import {
 export interface IdaeDbAdapterInterface<T extends Document> {
 	createIndex(fieldOrSpec: IndexSpecification, options?: CreateIndexesOptions): Promise<string>;
 	create(data: Partial<T>): Promise<T>;
-	findById(id: string): Promise<T>;
+	findById(id: string): Promise<T[]>;
 	find(params: IdaeDbParams<T>): Promise<T[]>;
 	findOne(params: IdaeDbParams<T>): Promise<T | null>;
 	update(id: string, updateData: Partial<T>): Promise<any>;
 	updateWhere(params: IdaeDbParams<T>, updateData: Partial<T>): Promise<any>;
 	deleteById(id: string): Promise<any>;
 	deleteWhere(params: IdaeDbParams<T>): Promise<{ deletedCount?: number }>;
+	transaction<TResult>(callback: (session: any) => Promise<TResult>): Promise<TResult>;
+
+	connect(uri: string): Promise<any>;
+	getDb(client: any, dbName: string): any;
+	close(client: any): Promise<void>;
 }
 
 export interface IdaeDbParams<T extends object = Record<string, unknown>> {
