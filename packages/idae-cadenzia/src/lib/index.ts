e7f7e3cad83101d039c4ef5988402db0d1475cd2
medyll <43032494+medyll@users.noreply.github.com>
2025-02-08 13:56:50 +0100

    chore: cadenzia initial

--- packages/idae-cadenzia/src/lib/index.ts
+++ packages/idae-cadenzia/src/lib/index.ts
@@ -1,1 +1,9 @@
-// place files you want to import through the `$lib` alias in this folder.
+// auto exports of entry components
+export * from '$lib/types/types.js';
+export * from '$lib/functions/rules.js';
+export * from '$lib/functions/functions.svelte.js';
+export * from '$lib/constants/constants.js';
+export { default as ChordVisualization } from '$lib/components/ChordVisualization.svelte';
+export { default as ChordTable } from '$lib/components/ChordTable.svelte';
+export { default as CadencePanel } from '$lib/components/CadencePanel.svelte';
+export { default as App } from '$lib/components/App.svelte';
