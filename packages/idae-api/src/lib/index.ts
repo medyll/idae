dfbcf5e38bed96d8391a735a6bba105a4f9cabbb
medyll <43032494+medyll@users.noreply.github.com>
2025-03-03 22:02:09 +0100

    fix(idae-api, idae-slotui): corriger les exportations et renommer les classes de menu

--- packages/idae-api/src/lib/index.ts
+++ packages/idae-api/src/lib/index.ts
@@ -1,11 +1,11 @@
 // auto exports of entry components
 export * from '$lib/server/IdaeApi.js';
 export * from '$lib/config/routeDefinitions.js';
-export * from '$lib/@types/types.js';
 export * from '$lib/client/IdaeApiClientRequest.js';
 export * from '$lib/client/IdaeApiClientConfig.js';
 export * from '$lib/client/IdaeApiClientCollection.js';
 export * from '$lib/client/IdaeApiClient.js';
+export * from '$lib/@types/types.js';
 export * from '$lib/server/services/AuthService.js';
 export * from '$lib/server/middleware/databaseMiddleware.js';
 export * from '$lib/server/middleware/authMiddleware.js';
