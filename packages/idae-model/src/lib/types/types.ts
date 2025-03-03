ef330a5d096cb66199d2f57631da3a2e6e8a7dde
medyll <43032494+medyll@users.noreply.github.com>
2025-03-03 04:00:50 +0100

    feat(idae-model): refactor Fk type and enhance AppSchemeModel with createModel function

--- packages/idae-model/src/lib/types/types.ts
+++ packages/idae-model/src/lib/types/types.ts
@@ -61,15 +61,62 @@ export interface AppSSchemeView extends Shared {
 	};
 }
 
+interface FkModel {
+	id: schemeId;
+	code: schemeCode;
+	order?: schemeOrder;
+	required?: boolean;
+	readonly?: boolean;
+	multiple?: boolean;
+}
 /** used for FKs */
-type Fk<T> = Record<
-	keyof T,
-	{
-		id: schemeId;
-		code: schemeCode;
-		order?: schemeOrder;
-		required?: boolean;
-		readonly?: boolean;
-		multiple?: boolean;
+type Fk<T> = Record<keyof T, FkModel>;
+
+export interface AppSchemeModel<T> extends Partial<Shared> {
+	[id: string]: any;
+	fk: Fk<T>;
+}
+
+function createAppSchemeBase(data: AppSchemeBase) {
+	return data;
+}
+
+function createAppSchemeType(data: AppSchemeType) {
+	return data;
+}
+
+export function createModel<T extends AppSchemeModel<T>>(model: string, data: T) {
+	Object.keys(data.fk).forEach((key) => {
+		data.fk[key] = createFk(data.fk[key]);
+	});
+	return data;
+}
+
+let test: AppSchemeModel<AppSchemeType>;
+
+const dataModel: AppScheme = {
+	code: 'exampleCode',
+	fk: {
+		appscheme_type: {
+			required: false,
+			readonly: false,
+			multiple: false
+		},
+		appscheme_base: {
+			required: false,
+			readonly: false,
+			multiple: false
+		}
 	}
->;
+};
+const fkData = createModel('AppSchemeType', dataModel); //?
+
+function createFk(fkModel: FkModel): FkModel {
+	// Assuming we need to initialize or modify the fkModel in some way
+	return {
+		...fkModel,
+		required: fkModel.required ?? false,
+		readonly: fkModel.readonly ?? false,
+		multiple: fkModel.multiple ?? false
+	};
+}
