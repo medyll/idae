35ed7a3a3c4025cbbdef232840b1ad6e520935df
medyll <43032494+medyll@users.noreply.github.com>
2025-04-04 21:34:49 +0200

    feat(idae-be): enhance timer handling with methods property and improved clearTimeout/clearInterval functionality

--- packages/idae-be/src/lib/modules/timers.ts
+++ packages/idae-be/src/lib/modules/timers.ts
@@ -23,6 +23,15 @@ export class TimersHandler implements CommonHandler<TimersHandler> {
 	constructor(element: Be) {
 		this.beElement = element;
 	}
+	methods: string[] | keyof TimersHandler = TimersHandler.methods;
+
+	valueOf(): unknown {
+		return {
+			methods: this.methods,
+			timer: this._timer,
+			interval: this._interval
+		};
+	}
 
 	handle(actions: TimerHandlerHandle): Be {
 		if (!actions) return this.beElement;
@@ -43,7 +52,15 @@ export class TimersHandler implements CommonHandler<TimersHandler> {
 	}
 
 	timeout(value: TimerHandlerHandle['timeout'], callback?: HandlerCallBackFn): Be {
-		this.beElement.eachNode((el: HTMLElement) => {
+		this.beElement.BeTimer = setTimeout(() => {
+			callback?.({
+				method: 'timeout',
+				fragment: this.beElement.BeTimer,
+				be: this.beElement,
+				root: this.beElement
+			});
+		}, value);
+		/* this.beElement.eachNode((el: HTMLElement) => {
 			const aug = be(el);
 			aug.BeTimer = setTimeout(() => {
 				callback?.({
@@ -53,45 +70,67 @@ export class TimersHandler implements CommonHandler<TimersHandler> {
 					root: this.beElement
 				});
 			}, value);
-		});
+		}); */
 
 		return this.beElement;
 	}
-	interval(value: TimerHandlerHandle['interval'], callback?: HandlerCallBackFn) {
-		this.beElement.eachNode((el: HTMLElement) => {
+	interval(value: TimerHandlerHandle['interval'], callback?: HandlerCallBackFn): Be {
+		this.beElement.BeInterval = setInterval(() => {
+			callback?.({
+				method: 'interval',
+				fragment: this.beElement.BeInterval,
+				be: this.beElement,
+				root: this.beElement
+			});
+		}, value);
+		/* this.beElement.eachNode((el: HTMLElement) => {
 			const aug = be(el);
-			aug.BeTimer = setInterval(() => {
+			aug.BeInterval = setInterval(() => {
 				callback?.({
 					method: 'interval',
-					fragment: aug.BeTimer,
+					fragment: aug.BeInterval,
 					be: be(el),
 					root: this.beElement
 				});
 			}, value);
-		});
+		}); */
 
 		return this.beElement;
 	}
-	clearTimeout(callback?: HandlerCallBackFn) {
-		this.beElement.eachNode((el: HTMLElement) => {
-			clearTimeout(be(el).BeTimer);
-			callback?.({
-				method: 'clearTimeout',
-				fragment: be(el).BeTimer,
-				be: be(el),
-				root: this.beElement
-			});
+	clearTimeout(callback?: HandlerCallBackFn): Be {
+		clearTimeout(this.beElement?.BeTimer);
+		this.beElement.BeTimer = null;
+		/* this.beElement.eachNode((el: HTMLElement) => {
+			const aug = be(el);
+			if (aug.BeTimer !== null) {
+				clearTimeout(aug.BeTimer);
+				aug.BeTimer = null;
+			}
+		}); */
+		callback?.({
+			method: 'clearTimeout',
+			fragment: null,
+			be: this.beElement,
+			root: this.beElement
 		});
+		return this.beElement;
 	}
-	clearInterval(callback?: HandlerCallBackFn) {
-		this.beElement.eachNode((el: HTMLElement) => {
-			clearInterval(be(el).BeTimer);
-			callback?.({
-				method: 'clearInterval',
-				fragment: be(el).BeTimer,
-				be: be(el),
-				root: this.beElement
-			});
+	clearInterval(callback?: HandlerCallBackFn): Be {
+		clearInterval(this.beElement?.BeInterval);
+		this.beElement.BeInterval = null;
+		/* this.beElement.eachNode((el: HTMLElement) => {
+			const aug = be(el);
+			if (aug.BeInterval !== null) {
+				clearInterval(aug.BeInterval);
+				aug.BeInterval = null;
+			}
+		}); */
+		callback?.({
+			method: 'clearInterval',
+			fragment: null,
+			be: this.beElement,
+			root: this.beElement
 		});
+		return this.beElement;
 	}
 }
