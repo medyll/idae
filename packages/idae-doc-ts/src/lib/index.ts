80e95258e779db1707c8529dbdb5b732d383d615
medyll <43032494+medyll@users.noreply.github.com>
2025-04-08 02:48:29 +0200

    feat: enhance package.json and add README for Stator library demo

--- packages/idae-doc-ts/src/lib/index.ts
+++
@@ -1,7 +0,0 @@
-// auto exports of entry components
-export * from '$lib/project.txt';
-export * from '$lib/tsdoc/tsdoc.js';
-export * from '$lib/example/scan.js';
-export * from '$lib/example/dir/types.js';
-export * from '$lib/example/dir/example-2.js';
-export * from '$lib/example/dir/example-1.js';
