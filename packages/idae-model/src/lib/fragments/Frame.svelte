c4a6e340e98124b72691cfb97edb5b9cbb81ad1d
medyll <43032494+medyll@users.noreply.github.com>
2025-05-03 15:40:21 +0200

    feat(idae-model): add new Svelte components for confirmation, layout, and data display

---
+++ packages/idae-model/src/lib/fragments/Frame.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import { slide } from 'svelte/transition';
+
+	export let showPanel: boolean = true;
+	export let panelMode: 'expanded' | 'reduced' = 'expanded';
+</script>
+
+<div class="relative flex h-full gap-4">
+	{#if showPanel}
+		<div transition:slide={{ axis: 'x' }} class="paper h-64 w-64 overflow-auto overflow-x-hidden">
+			<slot name="leftNav" />
+		</div>
+	{/if}
+	<div class="flex-1">
+		<div class="flex h-full w-96 flex-col gap-1">
+			<slot />
+		</div>
+	</div>
+</div>
---
+++ packages/idae-model/src/lib/fragments/List.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	let className: string = '';
+	export let data: any[];
+	export { className as class };
+	export let naked = false;
+	export let title: string = '';
+</script>
+
+{#if naked}
+	{title}
+	{#each data ?? [] as item}
+		<slot {item} />
+	{/each}
+{:else}
+	{title}
+	<div class={className}>
+		{#each data ?? [] as item, idx}
+			<slot {item} {idx} />
+		{/each}
+	</div>
+{/if}
