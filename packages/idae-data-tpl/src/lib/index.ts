08db0c2198c604e79c2b6d68beab0a780e4d2adb
medyll <43032494+medyll@users.noreply.github.com>
2025-06-06 10:43:01 +0200

    chore: packaging

--- packages/idae-data-tpl/src/lib/index.ts
+++ packages/idae-data-tpl/src/lib/index.ts
@@ -1,4 +1,9 @@
 // auto exports of entry components
+export * from '$lib/fromIdaeMongo/types.js';
+export * from '$lib/fromIdaeMongo/schemeModelClass.js';
+export * from '$lib/fromIdaeMongo/SchemeModel.js';
+export * from '$lib/fromIdaeMongo/SchemeFieldClass.js';
+export * from '$lib/fromIdaeMongo/Entity.js';
 export * from '$lib/dataTemplate/validator.js';
 export * from '$lib/dataTemplate/types.js';
 export * from '$lib/dataTemplate/dataTemplate.js';
