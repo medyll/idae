1b079431273aae0fe92fddaeb0fbe95254793f06
medyll <43032494+medyll@users.noreply.github.com>
2025-06-15 16:41:23 +0200

    refactor(idae-api-nest): restructure fk property to use Mongoose schema and define FkValue type

--- packages/idae-api-nest/src/idae/schema/appscheme.schema.ts
+++ packages/idae-api-nest/src/idae/schema/appscheme.schema.ts
@@ -1,23 +1,45 @@
-import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { Document } from 'mongoose';
-import { AppSchemeBase } from './appschemebase.schema.js';
-import { AppSchemeType } from './appschemetype.schema.js';
-import { AppSchemeGroup } from './appschemegroup.schema.js';
+import { Prop, Schema, SchemaFactory,  } from '@nestjs/mongoose';
+import { Document,Schema as MongooseSchema } from 'mongoose';
+
+export type FkValue = {
+  code: string;
+  multiple: boolean;
+  order: number;
+};
 
 @Schema({ timestamps: true })
 export class AppScheme extends Document {
+  @Prop({ required: true })
+  idappscheme: number;
+
   @Prop({ required: true })
   name: string;
 
   @Prop({ type: String })
   code: string;
 
-  @Prop({ type: Object })
-  fk: {
-    appscheme_base: AppSchemeBase;
-    appscheme_type: AppSchemeType;
-    appscheme_group: AppSchemeGroup;
-  };
+  @Prop({ type: Number })
+  order: number
+
+  @Prop({ type: String })
+  icon: string
+
+  @Prop({type:String})
+  color: string;
+
+  @Prop({
+    type: Map,
+    of: new MongooseSchema(
+      {
+        code: { type: String, required: true },
+        multiple: { type: Boolean, required: true },
+        order: { type: Number, required: true },
+      },
+      { _id: false }
+    ),
+    default: {},
+  })
+  fk: Map<string, FkValue>;
 }
 
 export const AppSchemeSchema = SchemaFactory.createForClass(AppScheme);
