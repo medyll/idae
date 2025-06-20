c0a7eddccb9d4119e86f98e0e6d27e2150b6f801
medyll <43032494+medyll@users.noreply.github.com>
2025-06-20 14:19:47 +0200

    fix(idae-main): Update example usage to include 'nest-test' in the generateReadme function call

--- scripts/generate-readme.js
+++ scripts/generate-readme.js
@@ -159,4 +159,4 @@ function generateReadme(blacklist = []) {
 // Exemple d'utilisation :
 // generateReadme(['idae-db', 'idae-chroma']);
 
-generateReadme(['ai-bash','idae-chroma','idae-cadenzia','idae-model','idae-data-tpl','idae-api-nest']);
+generateReadme(['ai-bash','idae-chroma','idae-cadenzia','idae-model','idae-data-tpl','idae-api-nest','nest-test']);
