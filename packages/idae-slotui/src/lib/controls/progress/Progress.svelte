dd6451fc54b4518862425791ede9797be509a06b
medyll <43032494+medyll@users.noreply.github.com>
2026-01-17 22:17:41 +0100

    refactor(idae-slotui): migrate progress component styles to Tailwind utility classes

--- packages/idae-slotui/src/lib/controls/progress/Progress.svelte
+++ packages/idae-slotui/src/lib/controls/progress/Progress.svelte
@@ -19,10 +19,8 @@
 	const precWidth = $derived((value / percentBase) * 100);
 </script>
 
-<div bind:this={element} class="progress {className} w-large" {style} {title}>
-	<div class="progress-gouge" style="{attr}:{precWidth}%;{attrTickness}:0.5rem"></div>
+<div bind:this={element} class="progress {className} w-large border border-[var(--progress-border)] rounded-[var(--progress-radius)] p-px" {style} {title}>
+	<div class="progress-gouge bg-[var(--progress-gouge-background)] rounded-[var(--progress-radius)] border border-[var(--progress-gouge-border)] transition-all max-w-full" style="{attr}:{precWidth}%;{attrTickness}:0.5rem"></div>
 </div>
 
-<style lang="scss">
-	@use './progress.scss';
-</style>
+
