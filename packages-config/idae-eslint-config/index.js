c49249ba513937e62f2f842981b4341703df5cf6
medyll <43032494+medyll@users.noreply.github.com>
2026-01-23 10:43:31 +0100

    refactor: Update ESLint configuration to use baseConfig and reorganize dependencies in package.json

--- packages-config/idae-eslint-config/index.js
+++ packages-config/idae-eslint-config/index.js
@@ -1,32 +1,39 @@
-eslintPatch();
+import js from "@eslint/js";
+import ts from "typescript-eslint";
+import svelte from "eslint-plugin-svelte";
+import prettier from "eslint-config-prettier";
+import globals from "globals";
 
-export default {
-  root: true,
-  extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:svelte/recommended",
-    "prettier",
-  ],
-  parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint"],
-  parserOptions: {
-    sourceType: "module",
-    ecmaVersion: 2020,
-    extraFileExtensions: [".svelte", ".svelte.ts"],
+/** @type {import('eslint').Linter.Config[]} */
+export const baseConfig = [
+  js.configs.recommended,
+  ...ts.configs.recommended,
+  ...svelte.configs["flat/recommended"],
+  prettier,
+  ...svelte.configs["flat/prettier"],
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+      ecmaVersion: 2020,
+      sourceType: "module",
+    },
   },
-  env: {
-    browser: true,
-    es2017: true,
-    node: true,
-  },
-  overrides: [
-    {
-      files: ["*.svelte"],
-      parser: "svelte-eslint-parser",
+  {
+    files: ["**/*.svelte", "**/*.svelte.ts"],
+    languageOptions: {
+      parser: svelte.parser, // Important pour Svelte
       parserOptions: {
-        parser: "@typescript-eslint/parser",
+        parser: ts.parser,
+        extraFileExtensions: [".svelte", ".svelte.ts"],
       },
     },
-  ],
-};
+  },
+  {
+    ignores: ["build/", ".svelte-kit/", "dist/"],
+  },
+];
+
+export default baseConfig;
