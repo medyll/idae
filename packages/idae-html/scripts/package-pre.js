88db786924b5032d10b94847358271bdd9141e86
medyll <43032494+medyll@users.noreply.github.com>
2026-01-28 16:57:05 +0100

    feat: update package dependencies to use @medyll/idae-shared and refactor import paths

--- packages/idae-html/scripts/package-pre.js
+++ packages/idae-html/scripts/package-pre.js
@@ -1,4 +1,4 @@
 // Created scripts/package-pre.js
 
-import { MakeLibIndex } from '../../shared/scripts/indexIfy.js';
+import { MakeLibIndex } from '@medyll/idae-shared';
 new MakeLibIndex().makeIndexFile();
--- packages/idae-slotui/scripts/package-pre.js
+++ packages/idae-slotui/scripts/package-pre.js
@@ -1,5 +1,5 @@
 import { SCSSConverter } from './release-css.js';
-import { MakeLibIndex } from '../../shared/scripts/indexIfy.js';
+import { MakeLibIndex } from '@medyll/idae-shared';
 
 const converter = new SCSSConverter('./src/lib', './src/lib/slotui-css');
 
