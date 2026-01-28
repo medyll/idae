db9e03722542e781c8ba310164c6ccf79b54b343
medyll <43032494+medyll@users.noreply.github.com>
2026-01-28 15:42:15 +0100

    chore: release v0.0.2

--- packages/idae-query/src/lib/index.ts
+++ packages/idae-query/src/lib/index.ts
@@ -1,6 +1,6 @@
 // auto exports of entry components
 export * from '$lib/types.js';
-export * from '$lib/resultSet/resultset.js';
 export * from '$lib/query/query.js';
-export * from '$lib/path/pathResolver.js';
+export * from '$lib/resultSet/resultset.js';
 export * from '$lib/operators/operators.js';
+export * from '$lib/path/pathResolver.js';
