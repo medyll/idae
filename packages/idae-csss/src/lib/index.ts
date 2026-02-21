2ac549acad6ace1681e4434828617d870a34f23a
medyll <43032494+medyll@users.noreply.github.com>
2026-02-21 19:56:54 +0100

    Refactor exports in index.ts for improved organization and clarity

--- packages/idae-csss/src/lib/index.ts
+++ packages/idae-csss/src/lib/index.ts
@@ -1,8 +1,8 @@
 // auto exports of entry components
-export * from '$lib/csss/types.js';
-export * from '$lib/csss/runtime.js';
-export * from '$lib/csss/plugin.js';
-export * from '$lib/csss/parser.js';
-export * from '$lib/csss/meta.js';
-export * from '$lib/csss/demo.csss';
 export * from '$lib/csss/csss.js';
+export * from '$lib/csss/demo.csss';
+export * from '$lib/csss/meta.js';
+export * from '$lib/csss/parser.js';
+export * from '$lib/csss/plugin.js';
+export * from '$lib/csss/runtime.js';
+export * from '$lib/csss/types.js';
--- packages/idae-mongo/src/lib/index.ts
+++ packages/idae-mongo/src/lib/index.ts
@@ -1,6 +1,6 @@
 // auto exports of entry components
-export * from '$lib/data/types.js';
-export * from '$lib/data/schemeModelClass.js';
-export * from '$lib/data/SchemeModel.js';
-export * from '$lib/data/SchemeFieldClass.js';
 export * from '$lib/data/Entity.js';
+export * from '$lib/data/SchemeFieldClass.js';
+export * from '$lib/data/SchemeModel.js';
+export * from '$lib/data/schemeModelClass.js';
+export * from '$lib/data/types.js';
